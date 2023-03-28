b50cfaac9cf70bc6f242a897f34b3642f045e589
Oscar Ernesto Pérez Cham <127453444+operezcham-alumnos@users.noreply.github.com>
2023-03-27 21:04:28 -0600

    Mensajería 2.0
    Incluye las correcciones de la clase 27/03/2023

--- chat/mensajes.js
+++ chat/mensajes.js
@@ -1,81 +1,57 @@
-const mensajeContainer = document.getElementById('mensaje-container');
+const http = require('http');
+const PORT = 80;
+const fs = require('fs');
 
-function cargarMensajes() {
-    fetch('mensajes.json')
-        .then(response => response.json())
-        .then(data => {
-            mensajeContainer.innerHTML = '';
-            data.forEach(mensaje => agregarMensaje(mensaje));
-        });
-}
+const mensajes = [
+    {
+        "usuario": "Erik",
+        "texto": "Hola, ¿cómo estás?",
+        "fecha": "2022-03-15T12:30:00Z"
+    },
+    {
+        "usuario": "Andy",
+        "texto": "¡Hola Erik! Estoy bien, ¿y tú?",
+        "fecha": "2022-03-15T12:31:00Z"
+    },
+    {
+        "usuario": "Erik",
+        "texto": "Qué bueno, ¿qué planes tienen para hoy?",
+        "fecha": "2022-03-15T12:33:00Z"
+    },
+    {
+        "usuario": "Andy",
+        "texto": "Pensábamos ir al cine, ¿te animas?",
+        "fecha": "2022-03-15T12:34:00Z"
+    }
+]
 
-function agregarMensaje(mensaje) {
-    const div = document.createElement('div');
-    const usuario = document.createElement('h4');
-    usuario.textContent = mensaje.usuario;
-    const texto = document.createElement('p');
-    texto.textContent = mensaje.texto;
-    const fecha = document.createElement('span');
-    fecha.textContent = new Date(mensaje.fecha).toLocaleString();
-    //const botonEliminar = document.createElement('button');
-    //botonEliminar.textContent = 'Eliminar';
-    //botonEliminar.addEventListener('click', () => eliminarMensaje(mensaje));
-    div.appendChild(usuario);
-    div.appendChild(texto);
-    div.appendChild(fecha);
-    //div.appendChild(botonEliminar);
-    mensajeContainer.appendChild(div);
-}
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+        const paginaHtmlPath = './index.html';
+        const paginaHtml = fs.readFileSync(paginaHtmlPath, 'utf8');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(paginaHtml);
+    } else if (req.method === 'POST' && req.url === '/mensajes') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            const nuevoMensaje = JSON.parse(body);
+            //nuevoMensaje.id = usuarios[usuarios.length-1].id + 1;
+            mensajes.push(nuevoMensaje);
+            res.statusCode = 201;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(mensajes));
+        });
+    } else if (req.method === 'GET' && req.url === '/mensajes') {
+        res.statusCode = 201;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(mensajes));
+    }
+});
 
-function crearMensaje() {
-    const usuario = document.getElementById('usuario').value;
-    const texto = document.getElementById('texto').value;
-    const fecha = new Date().toISOString();
-    const nuevoMensaje = {
-        usuario: usuario,
-        texto: texto,
-        fecha: fecha
-    };
-    fetch('mensajes.json')
-        .then(response => response.json())
-        .then(data => {
-            data.push(nuevoMensaje);
-            fetch('mensajes.json', {
-                method: 'PUT',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(() => {
-                    agregarMensaje(nuevoMensaje);
-                    document.getElementById('usuario').value = '';
-                    document.getElementById('texto').value = '';
-                });
-        });
-}
-/*
-function eliminarMensaje(mensaje) {
-    fetch('mensajes.json')
-        .then(response => response.json())
-        .then(data => {
-            const indice = data.findIndex(m => m.fecha === mensaje.fecha);
-            if (indice !== -1) {
-                data.splice(indice, 1);
-                fetch('mensajes.json', {
-                    method: 'PUT',
-                    body: JSON.stringify(data),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                    .then(() => {
-                        const divAEliminar = mensajeContainer.querySelector(`div:contains("${mensaje.texto}")`);
-                        divAEliminar.remove();
-                    });
-            }
-        });
-}
-*/
-cargarMensajes();
-crearMensaje();
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
